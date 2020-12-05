14bd49626348ffb42a433dda1b1626e396ad4769
petermunachi <alimunachipeter@gmail.com>
2020-12-05 18:55:44 +0100

    setting up total supply

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -6,8 +6,11 @@ contract DappToken {
 
   uint256 public totalSupply;
 
-  constructor () public {
-    totalSupply = 1000000;  
+  mapping (address=>uint256) public balanceOf;
+
+  constructor (uint256 _initialSupply) public {
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;  
   }
 
 
