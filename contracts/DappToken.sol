2509696d388974ee03dd8669115bb56a33699a46
petermunachi <alimunachipeter@gmail.com>
2020-12-18 00:08:45 +0100

    3. Delegated transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -15,7 +15,17 @@ contract DappToken {
     uint256 indexed _value
   );
 
+  event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 indexed _value
+  );
+
+  // transfer
+
   mapping (address=>uint256) public balanceOf;
+  // allowance
+  mapping (address=> mapping (address=>uint256)) public allowance;
 
   constructor (uint256 _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
@@ -34,6 +44,34 @@ contract DappToken {
 
     return true;
   }
+
+  // approve
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+
+    allowance[msg.sender][_spender] = _value; 
+    emit Approval(msg.sender, _spender, _value);
+
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require(_value <= balanceOf[_from], "Insufficient funds");
+    require(_value <= allowance[_from][msg.sender], "Amount allocated to you is lessthan the requested amount");
+
+    //change the balance
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+
+    //update the allowance
+    allowance[_from][msg.sender] -= _value;
+
+    //transfer event
+    emit Transfer(_from, _to, _value);
+
+    //return a boolean
+    return true;
+  }
+
 
 
 }
